41c658be8846727d8875f2e206a1e26de758d265
manisu <35994839+manisu@users.noreply.github.com>
2018-02-28 15:54:24 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("\n the given number is a multiple of 7");
+}
+else
+printf("\n the given number is not a multiple of 7");
+getch();
+}
